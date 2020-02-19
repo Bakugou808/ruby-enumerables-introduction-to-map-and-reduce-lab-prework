249bd3a6ead19f8bb143cfc8b98a8ab8f2b02e85
Janu Sung <janu.m.sung@gmail.com>
2020-02-19 02:35:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,51 @@
 # My Code here....
+
+def map_to_negativize(source_array) 
+  negatived = []
+  
+  i = 0 
+  
+  while i < source_array.length do 
+    negatived << -(source_array[i])
+    i += 1 
+  end 
+  negatived
+end 
+
+def map_to_no_change(source_array) 
+  no_change = []
+  
+  i = 0 
+  
+  while i < source_array.length do 
+    no_change << source_array[i]
+    i += 1 
+  end 
+  
+  no_change
+end 
+
+def map_to_double(source_array) 
+  doubled = []
+  
+  i = 0 
+  
+  while i < source_array.length do 
+    doubled << 2 * (source_array[i])
+    i += 1 
+  end 
+  
+  doubled 
+end 
+
+def map_to_square(source_array) 
+  squared = []
+  
+  i = 0 
+  
+  while i < source_array.length do 
+    squared.push((source_array[i])**2)
+    i += 1 
+  end 
+  squared 
+end 
